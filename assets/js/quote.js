2014b914a7b60ef87305d21ac8a1e7fb0d29d705
jerismith32 <jerismith32@gmail.com>
2021-05-23 07:41:07 -0400

    updated Quote Container Section in HTML and defined const in quote.js

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,9 +1,31 @@
-//Create a variable to link to the inspirational quote div in HTML
-var inspirationQuote = document.querySelector("#inspirationQuote");
+const quote = document.querySelector("#quote");
+const author = document.querySelector("#author");
+const quoteBtn = document.querySelector("#quoteBtn");
 
-var getQuoteInfo = function(event) {
-    fetch ("https://zenquotes.io/api/today")
-    .then(function(response) {
-        console.log(response);
-    })
-}
+// //Create a variable to link to the inspirational quote div in HTML
+// var inspirationQuote = document.querySelector("#inspirationQuote");
+
+
+// var getQuoteInfo = function(event) {
+//    console.log(event);
+//     //Sets up the connection between ZenQuotes API (pulling the quote of the day)
+//     fetch ("https://zenquotes.io/api/today")
+//     .then(function(response) {
+//         console.log(response);
+//         if (response.ok) {
+//             response.json().then(function(data) {
+//                 console.log(data);
+//                 displayQuoteInfo(data);
+//             });
+//         } else {
+//             alert("Error, please select the Quote of the Day Button");
+//             }
+//     })
+//     .catch(function(error) {
+//         alert("Unable to connect to ZenQuotes");
+//     });
+// }
+
+// inspirationQuote.addEventListener('submit', getQuoteInfo);
+
+
