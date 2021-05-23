26088b724aa406c19f30e7989e464db75651d437
seanway28 <seanway28@gmail.com>
2021-05-23 18:48:49 -0400

    updated video options

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -1,182 +1,29 @@
-{
-    "kind": "youtube#searchListResponse",
-    "etag": "gwMM78IzJTI3FwCS0GzXcfIkdnw",
-    "nextPageToken": "CAUQAA",
-    "regionCode": "US",
-    "pageInfo": {
-      "totalResults": 1000000,
-      "resultsPerPage": 5
-    },
-    "items": [
-      {
-        "kind": "youtube#searchResult",
-        "etag": "5P8BqD12r-yTYdgJgK2f-QTqi18",
-        "id": {
-          "kind": "youtube#video",
-          "videoId": "d4S4twjeWTs"
-        },
-        "snippet": {
-          "publishedAt": "2017-03-26T08:00:08Z",
-          "channelId": "UCFKE7WVJfvaHW5q283SxchA",
-          "title": "Meditation For Inner Peace - Yoga With Adriene",
-          "description": "Dive into meditation this Spring, go inward to focus on what feels good in mind and body. Want to have a good body? Tend to the mind. This 10 min practice is a ...",
-          "thumbnails": {
-            "default": {
-              "url": "https://i.ytimg.com/vi/d4S4twjeWTs/default.jpg",
-              "width": 120,
-              "height": 90
-            },
-            "medium": {
-              "url": "https://i.ytimg.com/vi/d4S4twjeWTs/mqdefault.jpg",
-              "width": 320,
-              "height": 180
-            },
-            "high": {
-              "url": "https://i.ytimg.com/vi/d4S4twjeWTs/hqdefault.jpg",
-              "width": 480,
-              "height": 360
-            }
-          },
-          "channelTitle": "Yoga With Adriene",
-          "liveBroadcastContent": "none",
-          "publishTime": "2017-03-26T08:00:08Z"
-        }
+function getVideo() {
+    $.ajax({
+      type: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      data: {
+          key: 'AIzaSyCeRY-J15v7Uhjb4XwHh6ylk-N3s3LN7Vc',
+          q: $("#video").val(),
+          part: 'snippet',
+          maxResults: 6,
+          type: 'video',
+          videoEmbeddable: true,
       },
-      {
-        "kind": "youtube#searchResult",
-        "etag": "eSrgufksGzxqhhx2AEmDhKqDR3Q",
-        "id": {
-          "kind": "youtube#video",
-          "videoId": "rrLkhg3fA0M"
-        },
-        "snippet": {
-          "publishedAt": "2020-07-01T16:15:01Z",
-          "channelId": "UCIXZNvQNioXFkc_Z-dfprrw",
-          "title": "Restorative Yoga + Meditation | No Props 35-Minute Relaxing Practice",
-          "description": "Experience deep rest and relaxation with this restorative yoga class. This will be a slow paced, gentle sequence where we hold the postures for 1-2 minutes.",
-          "thumbnails": {
-            "default": {
-              "url": "https://i.ytimg.com/vi/rrLkhg3fA0M/default.jpg",
-              "width": 120,
-              "height": 90
-            },
-            "medium": {
-              "url": "https://i.ytimg.com/vi/rrLkhg3fA0M/mqdefault.jpg",
-              "width": 320,
-              "height": 180
-            },
-            "high": {
-              "url": "https://i.ytimg.com/vi/rrLkhg3fA0M/hqdefault.jpg",
-              "width": 480,
-              "height": 360
-            }
-          },
-          "channelTitle": "Jessica Richburg",
-          "liveBroadcastContent": "none",
-          "publishTime": "2020-07-01T16:15:01Z"
-        }
-      },
-      {
-        "kind": "youtube#searchResult",
-        "etag": "x0H-v2zJAowqrWEvVFd_NN-549U",
-        "id": {
-          "kind": "youtube#video",
-          "videoId": "Nw2oBIrQGLo"
-        },
-        "snippet": {
-          "publishedAt": "2018-01-10T10:00:03Z",
-          "channelId": "UC-0CzRZeML8zw4pFTVDq65Q",
-          "title": "15 minute CALMING YOGA for Stress Relief and Anxiety",
-          "description": "WELCOME to your modern day yoga channel by Sarah Beth Yoga where you can find clear & fuss free yoga videos ranging from short 10 minute yoga ...",
-          "thumbnails": {
-            "default": {
-              "url": "https://i.ytimg.com/vi/Nw2oBIrQGLo/default.jpg",
-              "width": 120,
-              "height": 90
-            },
-            "medium": {
-              "url": "https://i.ytimg.com/vi/Nw2oBIrQGLo/mqdefault.jpg",
-              "width": 320,
-              "height": 180
-            },
-            "high": {
-              "url": "https://i.ytimg.com/vi/Nw2oBIrQGLo/hqdefault.jpg",
-              "width": 480,
-              "height": 360
-            }
-          },
-          "channelTitle": "SarahBethYoga",
-          "liveBroadcastContent": "none",
-          "publishTime": "2018-01-10T10:00:03Z"
-        }
-      },
-      {
-        "kind": "youtube#searchResult",
-        "etag": "XMUSR_LhrPxLjZvKmftXO7wc8j4",
-        "id": {
-          "kind": "youtube#video",
-          "videoId": "2qI32ZTb6vU"
-        },
-        "snippet": {
-          "publishedAt": "2021-05-21T12:55:36Z",
-          "channelId": "UCwobzUc3z-0PrFpoRxNszXQ",
-          "title": "🔴 Relaxing Music 24/7, Calm Music, Sleep Music, Stress Relief Music, Meditation Music, Study, Sleep",
-          "description": "The binaural beats embedded in our music for yoga will let you deepen your yoga meditation practice. Be sure to visit and subscribe to Yellow Brick Cinema to ...",
-          "thumbnails": {
-            "default": {
-              "url": "https://i.ytimg.com/vi/2qI32ZTb6vU/default_live.jpg",
-              "width": 120,
-              "height": 90
-            },
-            "medium": {
-              "url": "https://i.ytimg.com/vi/2qI32ZTb6vU/mqdefault_live.jpg",
-              "width": 320,
-              "height": 180
-            },
-            "high": {
-              "url": "https://i.ytimg.com/vi/2qI32ZTb6vU/hqdefault_live.jpg",
-              "width": 480,
-              "height": 360
-            }
-          },
-          "channelTitle": "Yellow Brick Cinema - Relaxing Music",
-          "liveBroadcastContent": "live",
-          "publishTime": "2021-05-21T12:55:36Z"
-        }
-      },
-      {
-        "kind": "youtube#searchResult",
-        "etag": "67fZ8j4OPvLfYGejYEU193B6Lk8",
-        "id": {
-          "kind": "youtube#video",
-          "videoId": "CqnWMPuyT0g"
-        },
-        "snippet": {
-          "publishedAt": "2019-09-22T05:00:01Z",
-          "channelId": "UCFKE7WVJfvaHW5q283SxchA",
-          "title": "Meditation for Self Love  |  Yoga With Adriene",
-          "description": "Join me for a 13 minute guided meditation, suitable for beginners and experienced meditators alike! This rescue remedy practice can be done seated or lying ...",
-          "thumbnails": {
-            "default": {
-              "url": "https://i.ytimg.com/vi/CqnWMPuyT0g/default.jpg",
-              "width": 120,
-              "height": 90
-            },
-            "medium": {
-              "url": "https://i.ytimg.com/vi/CqnWMPuyT0g/mqdefault.jpg",
-              "width": 320,
-              "height": 180
-            },
-            "high": {
-              "url": "https://i.ytimg.com/vi/CqnWMPuyT0g/hqdefault.jpg",
-              "width": 480,
-              "height": 360
-            }
-          },
-          "channelTitle": "Yoga With Adriene",
-          "liveBroadcastContent": "none",
-          "publishTime": "2019-09-22T05:00:01Z"
-        }
-      }
+      success: function(data){
+        console.log(data)
+        var max = data.items.length;
+        var min = 0;
+        // generate a random number between the min 0 and the max 6
+        var index = Math.floor(Math.random() * (max - min)) + min;
 
-    ]
+        $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[index].id.videoId)
+        $('h3').text(data.items[index].snippet.title)
+        $('.description').text(data.items[index].snippet.description)
+      },
+      error: function(response){
+          console.log("Request Failed");
+      }
+    });
+  }
+
