846a08c0a5b398722b26bf40960bdce7b3f673fb
jerismith32 <jerismith32@gmail.com>
2021-05-24 23:28:38 -0400

    updated horoscope.js with birthday manipulation

--- assets/js/horoscope.js
+++ assets/js/horoscope.js
@@ -1,4 +1,18 @@
-var formEL = document.querySelector('#my-form');
+// var formEL = document.querySelector('#my-form');
+// var formSubmitHander = function(event) {
+//     event.preventDefault();
+//     /*
+//     event.preventDefault() stops the browser from performing the default action the event wants it to do. 
+//     In the case of submitting a form, it prevents the browser from sending the form's input data to a URL, 
+//     as we'll handle what happens with the form input data ourselves in JavaScript.
+//     */
+//     console.log('form submitted!');
+//     var name = document.getElementById('name').value;
+//     console.log('name:', name);
+// }
+// formEL.addEventListener("submit", formSubmitHander);
+
+var monthFormEL = document.querySelector('#birthdayMonthForm');
 var formSubmitHander = function(event) {
     event.preventDefault();
     /*
@@ -6,114 +20,80 @@ var formSubmitHander = function(event) {
     In the case of submitting a form, it prevents the browser from sending the form's input data to a URL, 
     as we'll handle what happens with the form input data ourselves in JavaScript.
     */
-    console.log('form submitted!');
-    var dateofBirth = document.getElementById('dateofBirth').value;
-    console.log('dateofBirth:', dateofBirth);
+    console.log('Month form submitted!');
+    var birthMonth = document.getElementById('month').value;
+    console.log('month:', birthMonth);
 }
-formEL.addEventListener("submit", formSubmitHander);
+monthFormEL.addEventListener("submit", formSubmitHander);
 
-
-var userSunSigns = document.querySelector("#usersSign")
-
-
-
-function zodiac_sign(day, month){
-    let astro_sign="";
-           
-    // checks month and date within the
-    // valid range of a specified zodiac
-    if (month == "december"){
-               
-        if (day < 22)
-        astro_sign = "Sagittarius";
-        else
-        astro_sign ="capricorn";
-    }
-               
-    else if (month == "january"){
-        if (day < 20)
-        astro_sign = "Capricorn";
-        else
-        astro_sign = "aquarius";
-    }
-               
-    else if (month == "february"){
-        if (day < 19)
-        astro_sign = "Aquarius";
-        else
-        astro_sign = "pisces";
-    }
-               
-    else if(month == "march"){
-        if (day < 21)
-        astro_sign = "Pisces";
-        else
-        astro_sign = "aries";
-    }
-    
-    else if (month == "april"){
-        if (day < 20)
-        astro_sign = "Aries";
-        else
-        astro_sign = "taurus";
-    }
-               
-    else if (month == "may"){
-        if (day < 21)
-        astro_sign = "Taurus";
-        else
-        astro_sign = "gemini";
-    }
-               
-    else if( month == "june"){
-        if (day < 21)
-        astro_sign = "Gemini";
-        else
-        astro_sign = "cancer";
-    }
-               
-    else if (month == "july"){
-        if (day < 23)
-        astro_sign = "Cancer";
-        else
-        astro_sign = "leo";
-    }
-               
-    else if( month == "august"){
-        if (day < 23)
-        astro_sign = "Leo";
-        else
-        astro_sign = "virgo";
-    }
-               
-    else if (month == "september"){
-        if (day < 23)
-        astro_sign = "Virgo";
-        else
-        astro_sign = "libra";
-    }
-               
-    else if (month == "october"){
-        if (day < 23)
-        astro_sign = "Libra";
-        else
-        astro_sign = "scorpio";
-    }
-               
-    else if (month == "november"){
-        if (day < 22)
-        astro_sign = "scorpio";
-        else
-        astro_sign = "sagittarius";
-    }
-               
-        document.write(astro_sign);
+var dayFormEl = document.querySelector('#birthdayDayForm');
+var formSubmitHander = function(event) {
+    event.preventDefault();
+    /*
+    event.preventDefault() stops the browser from performing the default action the event wants it to do. 
+    In the case of submitting a form, it prevents the browser from sending the form's input data to a URL, 
+    as we'll handle what happens with the form input data ourselves in JavaScript.
+    */
+    console.log('Day form submitted!');
+    var birthDay = document.getElementById('day').value;
+    console.log('day:', birthDay);
 }
+dayFormEl.addEventListener("submit", formSubmitHander);
+
+
+function getSunSign (){
+	var bdayMonth = document.getElementById('month').value;
+    var bdayDay = document.getElementById('day').value; 
+    var sunSign = bdayMonth*100 + bdayDay;
+    console.log(sunSign);
+	if (sunSign <= 120){        
+		return "Capricorn";
+        //January 20
+	}else if (sunSign <= 219) { 
+		return "Aquarius";
+        //January 20-February 18
+	}else if (sunSign <= 320) { 
+		return "Pisces";
+        //February 19-March 20
+	}else if (sunSign <= 420) { 
+		return "Aries";
+        //March 21-April 19
+	}else if (sunSign <= 520) { 
+		return "Taurus";
+        //April 20-May 20
+	}else if (sunSign <= 621) { 
+		return "Gemini";
+        //May 21-June 20
+	}else if (sunSign <= 722) { 
+		return "Cancer";
+        //June 21-July 22
+	}else if (sunSign <= 822) { 
+		return "Leo";
+        //July 23-August 22
+	}else if (sunSign <= 921) { 
+		return "Virgo";
+        //August 23-September 22
+	}else if (sunSign <= 1022) { 
+		return "Libra";
+        //September 23-October 22
+	}else if (sunSign <= 1121) { 
+		return "Scorpio";
+        //October 23-November 21
+	}else if (sunSign <= 1221) { 
+		return "Sagittarius";
+        //November 22-December 21
+	}else{ 
+		return "Capricorn";
+        //December 22-January 19
+	}
+}
+
 
 $.ajax({
     type:'POST',
-    url:'https://aztro.sameerkumar.website?sign=aries&day=today',
+    url:'https://aztro.sameerkumar.website?sign=' +  + '&day=today',
     success:function(data){
     console.log(data);
     }
      })
+
